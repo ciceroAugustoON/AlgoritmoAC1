28297c142ebae4cb6114dbcbbab742cea27a9375
ciceroAugustoON <101056067+ciceroAugustoON@users.noreply.github.com>
2023-05-04 14:22:38 -0300

    Versão 1.0
    -Correção de erros, quando o usuário erra todas as tentativas.
    -Melhoria na organização.

--- TrabalhoAc1/src/trabalhoac1/Trabalho.java
+++ TrabalhoAc1/src/trabalhoac1/Trabalho.java
@@ -8,14 +8,16 @@ public class Trabalho {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         Random gerador = new Random();
 
         int aleatorio = gerador.nextInt(101);
+
         int palpuser = -1;
         int tentativas;
 
         System.out.println("Bem-vindo ao jogo de adivinhação!");
-        System.out.println("Tente adivinhar o número que estou pensando entre 0 e 100.\n");
+        System.out.println("Tente adivinhar o número que estou pensando entre 0 e 100.");
 
         for (tentativas = 10; palpuser != aleatorio && tentativas > 0; tentativas--) {
 
@@ -25,9 +27,9 @@ public class Trabalho {
 
             if (tentativas > 1) {
                 if (palpuser < aleatorio) {
-                    System.out.println("\nTente um número maior!\n");
+                    System.out.println("\nTente um número maior!");
                 } else if (palpuser > aleatorio) {
-                    System.out.println("\nTente um número menor!\n");
+                    System.out.println("\nTente um número menor!");
                 }
             }
         }
